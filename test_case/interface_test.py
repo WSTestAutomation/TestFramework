dd0e0a6ab18f75ce0ff36726b7a2669af9db02ff
Ava Liu (Wicresoft) <v-avliu@microsoft.com>
2020-07-30 11:11:11 +0800

    add interface framework

---
+++ test_case/interface_test.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+
+import time
+import unittest
+import os
+from BeautifulReport.BeautifulReport import BeautifulReport
+from utils.base_runner import BaseWebTestCase
+from common.interface_request import get_text
+import pandas as pd
+from utils.base_log import Log
+logging = Log().get_logger()
+
+class AppInsights(BaseWebTestCase):
+    
+    @BeautifulReport.add_test_img('test_new_ai{}'.format(time.strftime('%Y%m%d%H%M%S')))
+    def test_new_ai(self):
+        logging.info("Create csv file")
+        base_dir = os.path.dirname(os.path.dirname(__file__))
+        test_data_path = os.path.join(base_dir, 'test_data')
+        input_file = os.path.join(test_data_path, 'test1.csv')
+        logging.info("Open URL")
+
+        ele_json = get_text('url')
+        data = pd.DataFrame()
+        for i in range(len(ele_json)):
+            data = data.append(ele_json[i],ignore_index=True)
+            logging.info("Get the url%s data Done" % (i+1))
+
+        pd.DataFrame(data).to_csv(input_file)
+        logging.info("Success")
+
+if __name__ == '__main__':
+    unittest.main()
--- test_run/run_appinsights.py
+++ test_run/run_appinsights.py
@@ -10,7 +10,7 @@ base_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 test_cases_path = os.path.join(base_dir, 'test_case')
 report_path = os.path.join(base_dir, 'report')
 
-test_suite1 = unittest.TestLoader().discover(test_cases_path, pattern='appinsights.py')
+test_suite1 = unittest.TestLoader().discover(test_cases_path, pattern='interface_test.py')
 
 suite = unittest.TestSuite(test_suite1)
 result = BeautifulReport(suite)
