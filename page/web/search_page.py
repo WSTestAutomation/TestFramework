2e68a59c68b8b1be1a8bdeba6654da27af85dbcb
Ava Liu (Wicresoft) <v-avliu@microsoft.com>
2020-09-16 17:44:21 +0800

    modify search_page.py

--- page/web/search_page.py
+++ page/web/search_page.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 
 class SearchPage:
     first_search_input = (By.XPATH, '//input[@aria-label="Enter your search"]')
-    search_input = (By.XPATH, '//input[@placeholder="Search for videos ..."]')
+    search_input = (By.XPATH, '//input[contains(@placeholder, "Search for")]')
     check_title_text = (By.XPATH, '//span[contains(@class, "-title ng-binding ng-isolate-scope")]')
     check_people_title_text = (By.XPATH, '//a[@class="person-card"]')
     check_description_text = (By.XPATH, '//div[@class="item-description-content ng-binding ng-scope"]')
@@ -19,7 +19,7 @@ class SearchPage:
     click_People_button = (By.XPATH, '//button[@class="c-action-trigger nav-link nav-link-option-2"]')
     check_search_for_text = (By.XPATH, '//label[@class="c-label ng-binding"]')
     my_content_column = (By.XPATH, '//span[@class="link-label ng-binding"]')
-    my_content_video = (By.XPATH, '/a[@class="my-video-link"]')
+    my_content_video = (By.XPATH, '//a[@class="my-video-link"]')
     my_content_channels = (By.XPATH, '//button[@class="c-action-trigger nav-link nav-link-option-1"]')
     check_my_content_result_if_null = (By.XPATH, '//div[@class="ng-binding ng-scope"]')
     my_content_my_watchlists = (By.XPATH, '//button[@class="c-action-trigger nav-link nav-link-option-3"]')
