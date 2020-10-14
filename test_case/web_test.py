e2c7d30fc1b8a5f093f0fe51a506ca96f7a3e1da
XuWenzheng <wenzhengxu@hotmail.com>
2020-10-14 16:04:33 +0800

    Fix conflics before migrate from stream_master to master

--- test_case/web_test.py
+++ test_case/web_test.py
@@ -7,33 +7,24 @@ sys.path.append(os.getcwd())
 
 import time
 import unittest
-from businessview.web.common.login_business import LoginBusiness
+from businessview.web.common.login_business import simple_login
 from BeautifulReport.BeautifulReport import BeautifulReport
-from common.browser_engine import open_browser
 from utilstest.base_runner import BaseWebTestCase
 from utilstest.base_yaml import Yaml
 from common.browser_engine import Logger
 
 
 class web_test(BaseWebTestCase):
-    def __init__(self,*args,**kwargs):
-        unittest.TestCase.__init__(self,*args,**kwargs)
-        self.data = Yaml(Yaml.web_config_path).read()
-        self.env = self.data['env']
+    def __init__(self, *args, **kwargs):
+        BaseWebTestCase.__init__(self, *args, **kwargs)
 
-    @BeautifulReport.add_test_img('web_test_login_{}'.format(time.strftime('%Y%m%d%H%M%S')))
-    def test_login(self):
-        '''Scenario1: A demo for login'''
-        Logger.info("Test Start:")
-        Logger.info("Login to %s", self.env)
-        self.driver = open_browser(self.env,'chrome')
-        _loginBusiness = LoginBusiness(driver=self.driver)
-
-        res = _loginBusiness.login(self.data['user']['personal_account'], self.data['pwd']['personal_pwd'])
-        self.assertTrue(res, 'Able to sign in %s with account %s' %(self.env, self.data['user']['personal_account']))
-
-
-        Logger.info("Test End")
+    @BeautifulReport.add_test_img('test_mainpage{}'.format(time.strftime('%Y%m%d%H%M%S')))
+    def test_mainpage(self):
+        # 登录，需要改配置文件指定环境、浏览器及登录凭证
+        self.driver = simple_login()
+        time.sleep(1)
+        # 添加步骤及断言
+        # self.assertTrue()
         
 if __name__ == '__main__':
     unittest.main()
