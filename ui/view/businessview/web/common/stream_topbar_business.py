a02bb18c8ce6942ac45dc5ffd440e5f1efb5abb8
XuWenzheng <wenzhengxu@hotmail.com>
2020-11-24 12:52:16 +0800

    Adding files back

---
+++ ui/view/businessview/web/common/stream_topbar_business.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+# @Time    :
+# @Author  :
+# @File    : stream_topbar_business.py
+
+from ui.view.baseview.web.business_web import BusinessWebPage
+from ui.view.page.web.business.common.topbar_page import TopbarPage as Page
+from ui.lib.browser_engine import Logger
+
+
+class Stream_topbar_business(BusinessWebPage):
+    def __init__(self, driver):
+        BusinessWebPage.__init__(self=self, driver=driver)
+        self._page = Page()
+        self.discover_dropdown_menu_dict = {'Videos':   self._page.discover_video_link,
+                                            'channels': self._page.discover_channel_link,
+                                            'people':   self._page.discover_people_link,
+                                            'Groups':   self._page.discover_group_link
+                                            }
+
+    def click_topbar_button(self, fname):
+        if self.is_element_clickable(fname):
+            self.click(fname)
+        else:
+            element = self.find_element(*fname)
+            self.perform_javascript_click(element)
+
+    def click_topbar_link(self, lname):
+        if self.is_element_clickable(lname):
+            self.click(lname)
+        else:
+            element = self.find_element(*lname)
+            self.perform_javascript_click(element)
+
+    def click_topbar_button_home(self):
+        self.click_topbar_button(self._page.home_link)
+
+    def click_topbar_button_discover(self):
+        self.click_topbar_button(self._page.discover_button)
